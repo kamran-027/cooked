b3282ec93c8ed8279962518abe1c0707b1bc2844
Kamran Khan <kamrankhan027.kk@gmail.com>
2025-08-19 22:47:46 +0530

    Completed Tailwind & ShadCN setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <div>Cooked App Here!!</div>;
+  return (
+    <div className="w-screen h-screen flex flex-col gap-2 items-center justify-center bg-gray-100">
+      <div className="text-red-400 font-bold bg-purple-300 p-5 rounded-md ">
+        Cooked App Here!!
+      </div>
+      <Button className="cursor-pointer">Click Here!</Button>
+    </div>
+  );
 }
 
 export default App;
