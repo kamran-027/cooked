21dd4cf2d1d66c064113f2a7a04663220e4a644c
Kamran Khan <kamrankhan027.kk@gmail.com>
2025-08-29 23:13:42 +0530

    Added Footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-[#fdf3e9] text-white py-4">
+    <footer className="bg-[#fdf3e9] text-white py-4 shadow-2xl mt-auto">
       <div className="container mx-auto px-6 text-center">
         <p className="text-gray-800 text-sm">
           &copy; {new Date().getFullYear()} Cooked. All rights reserved.
